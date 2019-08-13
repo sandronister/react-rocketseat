2e4e8a9ebfec6dce4adb91e917319016482f1e9f
julio <sandronister@gmail.com>
2019-08-13 01:22:03 -0300

    first commit

---
+++ src/components/Main/index.js
@@ -0,0 +1,62 @@
+import React ,{Component} from 'react'
+import api from '../../services/api'
+import './style.css'
+
+export default class Main extends Component{
+
+    state={
+        products:[], 
+        productInfo:{}, 
+        page: 1
+    }
+
+    componentDidMount(){
+        this.loadProducts()
+    }
+
+    loadProducts = async(page=1)=>{
+        const response = await api.get(`products?page=${page}`)
+
+        const {docs,...productInfo} = response.data
+        this.setState({products:docs,productInfo,page})
+    }
+
+     nextPage = ()=>{
+
+        const {page,productInfo} = this.state
+
+        if(page == productInfo.pages) return
+
+        const pageNumber = page+1
+
+        this.loadProducts(pageNumber)
+    }
+
+    prevPage = ()=>{
+        const {page,productInfo} = this.state
+
+        if(page ==1) return
+
+        const pageNumber = page-1
+
+        this.loadProducts(pageNumber)
+    }
+
+    render(){
+        return (
+            <div className='product-list'>
+                {this.state.products.map(product=>(
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
+                 <div className="actions">
+                        <button onClick={this.prevPage}>Anterior</button>
+                        <button onClick={this.nextPage}>Próximo</button>
+                    </div>
+            </div>
+        )
+    }
+}
